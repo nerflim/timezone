1f3c5432fce67a66d4a2273cf39ee72ad6b8242c
Milfren <nerflimjohn@gmail.com>
2019-08-09 23:53:48 +1000

    responsive time and date

--- src/components/svg/Time.js
+++ src/components/svg/Time.js
@@ -1,20 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import moment from 'moment';
 import { TweenMax } from 'gsap';
 
 const Time = () => {
+	const [time, setTime] = useState(new Date());
 	useEffect(() => {
 		TweenMax.from('#time', 0, {
 			x: 50
 		});
+		setInterval(() => {
+			setTime(new Date());
+		});
 	}, []);
 	return (
 		<g id='time'>
-			<text id='time' transform='matrix(1 0 0 1 196.0918 219.1401)' fill='#393762' fontWeight='bold' fontSize='37.2097px'>
-				{moment().format('hh:mm A')}
+			<text id='time' fill='#393762' fontWeight='bold' fontSize='37.2097px' x='338' y='220' textAnchor='end'>
+				{moment(time).format('hh:mm A')}
 			</text>
-			<text transform='matrix(1 0 0 1 231.0199 248.3901)' fill='#393762' fontSize='16px'>
-				August 8, 2019
+			<text id='date' fill='#393762' fontSize='16px' x='336' y='248' textAnchor='end'>
+				{moment(time).format('MMMM DD, YYYY')}
 			</text>
 			<rect x='316.2' y='226.6' width='19.1' height='2.1' fill='#393762' />
 		</g>
